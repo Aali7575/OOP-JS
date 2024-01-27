1ef4d285e39746ca3a62186f7f71dd6f0b53b337
Aali7575 <tafadzwaaaliyahndoro@gmail.com>
2024-01-27 22:59:03 +0200

    just practising

---
+++ try.js
@@ -0,0 +1,14 @@
+class Human {
+    constructor(name, age, location) {
+        this.name = name;
+        this.age = age;
+        this.location = location;
+}
+}
+
+firstperson =  new Human("Tafadzwa", 22, "Zimbabwe");
+console.log(firstperson);
+
+secondperson = new Human("Samantha",17, "Zimbabwe");
+console.log(secondperson(this.name));
+
